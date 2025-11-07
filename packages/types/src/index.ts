feba71d03675ff99f20a1a2f2891d575a89c5e3a
Joshua <joshuamanzorolhagh@gmail.com>
2025-11-07 12:58:25 -0500

    set up the User, PublicUser, and AuthResponse data objects for API contracts.

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,10 +1,20 @@
 export interface User {
   id: string;
   username: string;
-  email: string;
+  email: string; // Private info
+  created: string;
+}
+
+
+export interface PublicUser {
+  id: string;
+  username: string;
+  profilePicUrl: string;
 }
 
 export interface AuthResponse {
   token: string;
   user: User;
 }
+
+
