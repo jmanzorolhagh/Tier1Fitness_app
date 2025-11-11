44bfd4c19b8226f9b7ead626238a081ffe58a865
Joshua <joshuamanzorolhagh@gmail.com>
2025-11-11 13:59:12 -0500

    Simple get api endpoint to sanity check. Verified working.

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 
@@ -9,3 +9,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/api', (req: Request, res: Response) => {
+  res.json({ message: 'Tier1Fitness API is running!' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(` Server is running on http://localhost:${PORT}`);
+});
