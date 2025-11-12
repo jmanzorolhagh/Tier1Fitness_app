a5b2da3887e157e5822c82d78a70c16512d21478
David Cano <davecano.244@gmail.com>
2025-11-11 22:54:57 -0500

    Added post screen skeleton, replaced in app.tsx for testing

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import Leaderboard from './src/leaderboard';
+import CreatePostScreen from './src/postScreen';
 
 export default function App() {
+    const id = "123"
   return (
-    <View style={{ flex: 1, paddingTop: 50 }}>
-      <Leaderboard />
+    <View style={{ flex: 1, paddingTop: 50 }}>;
+      <CreatePostScreen userId={id} />
     </View>
   );
 }
